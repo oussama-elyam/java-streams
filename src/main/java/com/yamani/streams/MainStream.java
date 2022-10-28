773ab79987ed9c52843e0c596ed434ff0817bb24
elyamart <elyamanioussama3@gmail.com>
2022-10-28 15:10:48 +0100

    stream sorted

--- src/main/java/com/yamani/streams/MainStream.java
+++ src/main/java/com/yamani/streams/MainStream.java
@@ -1,7 +1,10 @@
 package com.yamani.streams;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,31 +13,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainStream {
 
 	public static void main(String[] args) {
-		List<Laptop> people = getPeople();
+		List<Laptop> laptop = getLaptop();
 
 	    // 1:brute force approach 	    
-	    /*
+	    
 		List<Laptop> LINUXs = new ArrayList<>();
-	    for (Laptop Laptop : people) {
-	      if (Laptop.getOperatingSys().equals(OperatingSys.LINUX)) {
+	    for (Laptop Laptop : laptop) {
+	      if (Laptop.getOs().equals(OperatingSys.LINUX)) {
 	        LINUXs.add(Laptop);
 	      }
 	    }
 	    LINUXs.forEach(System.out::println);
-	    */
+	    
 	    //1:stream filter
 		System.out.println("-------------------Stream Filter----------------------");
-	    List<Laptop> LINUXs = people.stream()
+	    List<Laptop> linux = laptop.stream()
 	        .filter(Laptop -> Laptop.getOs().equals(OperatingSys.LINUX))
 	        .collect(Collectors.toList());
 
-	    LINUXs.forEach(System.out::println);
+	    linux.forEach(System.out::println);
+	    
+	    //2:stream Sort
+	    System.out.println("-------------------Stream sorted----------------------");
+	    //sorting by price if price is equals then sort by ram
+	    List<Laptop> sorted = laptop.stream()
+	        .sorted(Comparator.comparing(Laptop::getPrice).thenComparing(Laptop::getRam).reversed())
+	        .collect(Collectors.toList());
+
+	    sorted.forEach(System.out::println);
+
 	    }
 	
-	 private static List<Laptop> getPeople() {
+	 private static List<Laptop> getLaptop() {
 		    return List.of(
 		        new Laptop("laptopA",5000, 8, OperatingSys.MAC),
-		        new Laptop("laptopB",6000, 12, OperatingSys.WINDOWS),
+		        new Laptop("laptopB",10000, 12, OperatingSys.WINDOWS),
 		        new Laptop("laptopC",3500, 8, OperatingSys.LINUX),
 		        new Laptop("laptopD",8000,16, OperatingSys.MAC),
 		        new Laptop("laptopE",10000,16, OperatingSys.MAC),
